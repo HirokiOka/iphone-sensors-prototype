03a54b16fb6b3c612c38d6752adf7f65609a281d
hiroki <hiro_bass.0827@icloud.com>
2021-12-31 23:43:25 +0900

    init commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const aclX = document.getElementById("acl-x");
+const aclY = document.getElementById("acl-y");
+const aclZ = document.getElementById("acl-z");
+
+let acl = new Accelerometer({frequency: 60});
+acl.addEventListener('reading', () => {
+  aclX.textContent = 'aclX:' + acl.x;
+  aclY.textContent = 'aclY:' + acl.y;
+  aclZ.textContent = 'aclZ:' + acl.z;
+});
