b8c34c058d69afea7fc9e13b1f92bc537b4059f7
hiroki <hiro_bass.0827@icloud.com>
2022-03-31 15:12:05 +0900

    add js as script file

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-const aclX = document.getElementById("acl-x");
-const aclY = document.getElementById("acl-y");
-const aclZ = document.getElementById("acl-z");
+let aclX = document.getElementById("acl-x");
+let aclY = document.getElementById("acl-y");
+let aclZ = document.getElementById("acl-z");
 
-/*
-let acl = new Accelerometer({frequency: 60});
-acl.addEventListener('reading', () => {
-  aclX.textContent = 'aclX:' + acl.x;
-  aclY.textContent = 'aclY:' + acl.y;
-  aclZ.textContent = 'aclZ:' + acl.z;
+const btn = document.getElementById('btn');
+btn.addEventListener('click', e => {
+  onClick();
 });
 
-acl.start();
-*/
-window.addEventListener("devicemotion", (e) => {
-  const acl = e.acceleration;
-  aclX.textContent = 'aclX:' + acl.x;
-  aclY.textContent = 'aclY:' + acl.y;
-  aclZ.textContent = 'aclZ:' + acl.z;
-});
+function displayAcc(e) {
+    document.getElementById('msg').textContent = 'event'; 
+    aclX.textContent = 'aclX:' + e.acceleration.x;
+    aclY.textContent = 'aclY:' + e.acceleration.y;
+    aclZ.textContent = 'aclZ:' + e.acceleration.z;
+}
+
+function onClick() {
+  if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    DeviceMotionEvent.requestPermission()
+      .then(permissionState => {
+        if (permissionState === 'granted') {
+          window.addEventListener('devicemotion', e => {
+              displayAcc(e);
+          });
+        }
+      })
+      .catch(console.error);
+  } else {
+      console.log('requestPermission is not a function');
+  }
+}
