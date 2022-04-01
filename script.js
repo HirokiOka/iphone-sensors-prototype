81e4e4aa00a0d326c9c37515f0c51f82acf95055
hiroki <hiro_bass.0827@icloud.com>
2022-04-01 17:44:02 +0900

    refac

--- script.js
+++ script.js
@@ -8,21 +8,27 @@ const rotG = document.getElementById("gamma");
 
 const btn = document.getElementById('btn');
 
-
-function displayValues(e) {
+function getSensorValues(e) {
   const aclXVal = Math.floor(e.acceleration.x * 100) / 100;
   const aclYVal = Math.floor(e.acceleration.y * 100) / 100;
   const aclZVal = Math.floor(e.acceleration.z * 100) / 100;
   const rotAVal = Math.floor(e.rotationRate.alpha * 100) / 100;
   const rotBVal = Math.floor(e.rotationRate.beta * 100) / 100;
   const rotGVal = Math.floor(e.rotationRate.gamma * 100) / 100;
-  aclX.textContent = `aclX: ${aclXVal}`;
-  aclY.textContent = `aclY: ${aclYVal}`;
-  aclZ.textContent = `aclY: ${aclZVal}`;
+  const values = {
+    aclXVal, aclYVal, aclZVal, rotAVal, rotBVal, rotGVal
+  };
+  return values;
+}
 
-  rotA.textContent = `alpha: ${rotAVal}`;
-  rotB.textContent = `beta: ${rotBVal}`;
-  rotG.textContent = `gamma: ${rotGVal}`;
+function displaySensorValues(values) {
+  aclX.textContent = `aclX: ${values.aclXVal}`;
+  aclY.textContent = `aclY: ${values.aclYVal}`;
+  aclZ.textContent = `aclY: ${values.aclZVal}`;
+
+  rotA.textContent = `alpha: ${values.rotAVal}`;
+  rotB.textContent = `beta: ${values.rotBVal}`;
+  rotG.textContent = `gamma: ${values.rotGVal}`;
 }
 
 btn.addEventListener('click', async () => {
@@ -30,7 +36,8 @@ btn.addEventListener('click', async () => {
   const permission = await DeviceMotionEvent.requestPermission();
   if (permission !== 'granted') return;
   window.addEventListener('devicemotion', async (e) => {
-      displayValues(e);
+    const sensorValues = getSensorValues(e);
+    displaySensorValues(sensorValues);
   });
 });
 
