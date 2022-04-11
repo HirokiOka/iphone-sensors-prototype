dbe3cd402d07ce0a0be861914cbf75c1c4539bcd
hiroki <hiro_bass.0827@icloud.com>
2022-04-11 17:55:27 +0900

    check DeviceMotionEventAcceleration.x

--- exp/main.js
+++ exp/main.js
@@ -1,6 +1,17 @@
+const aclX = document.getElementById("acl-x");
+const aclY = document.getElementById("acl-y");
+const aclZ = document.getElementById("acl-z");
+const rotA = document.getElementById("alpha");
+const rotB = document.getElementById("beta");
+const rotG = document.getElementById("gamma");
+const msg = document.getElementById("msg");
+
+/*
 window.addEventListener('load', e => {
-  if (typeof DeviceMotionEvent.requestPermission !== 'function') return;
-  const permission = await DeviceMotionEvent.requestPermission();
-  if (permission !== 'granted') return;
-  window.alert('success');
+
 });
+*/
+
+setInterval(() => {
+  aclX.textContent = DeviceMotionEventAcceleration.x;
+}, 1000);
